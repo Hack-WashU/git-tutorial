84cd29a27d749eb50d6ac6f89ed2e78c90af0c74
Jack Heuberger <jackheuberger@wustl.edu>
2023-02-13 16:35:09 -0600

    finished react changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
+import Name from './components/Names.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} id="logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This website was made by the following Hack WashU members:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Name />
+        </header>
     </div>
   );
 }
---
+++ react-app/src/components/Names.js
@@ -0,0 +1,10 @@
+import "./Names.css"
+
+export default function Name() {
+    return (
+        <div>
+            <a id="jackh" href="https://github.com/jackheuberger">Jack Heuberger</a>
+        </div>
+    );
+}
+
