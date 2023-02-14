bec827db0939deb02c9a2d5f55dc1df34284b5e2
Jack Heuberger <jackheuberger@wustl.edu>
2023-02-13 18:53:41 -0600

    Added my name!

--- react-app/src/components/Names.js
+++ react-app/src/components/Names.js
@@ -3,7 +3,8 @@ import "./Names.css"
 export default function Name() {
     return (
         <div>
-            <a id="jackh" href="https://github.com/jackheuberger">Jack Heuberger</a>
+            <p><a id="jackh" href="https://github.com/jackheuberger">Jack Heuberger</a></p>
+            <p><a>Jack Test</a></p>
         </div>
     );
 }
